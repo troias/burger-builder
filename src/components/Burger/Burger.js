b6eeb602934e789f740f187b3992d87a0ca048ae
troy <troyflavel@gmail.com>
2021-02-20 21:01:42 +1300

    Added dynamic component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,13 +1,33 @@
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = ( props) => {
-    return(
+const burger = (props) => {
+
+    //  variable that returns a jsx element/array/s based on the key value 
+
+    const x = Object.keys(props.ingredients).map(x => {
+        return [...Array(props.ingredients[x])].map((q, i) =>{
+            return  <BurgerIngredient key={x + i} type={x} />
+        })
+    })
+   
+    //  gets keys 
+
+    //  return array of keys -
+
+    //  create new arrays of  based on the amount ob keys and the amount of values
+
+    //  put jsx elements into that array
+
+    // create and array for each key 
+
+    
+
+    
+    return (
         <div className={classes.Burger}> 
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="bacon" />
+         {x}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,12 +4,23 @@ import Aux from '../../hoc/Auxillary'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ingredients: {
+                salad: 2, 
+                bacon: 1, 
+                cheese: 3, 
+                meat: 4,
+            }
+        }
+    }
 
     render() {
         return (
             <Aux>
                 <div>
-                    <Burger/>
+                    <Burger ingredients={this.state.ingredients} />
                 </div>
                 <div> 
                     Build Controls 
