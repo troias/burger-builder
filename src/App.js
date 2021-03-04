ae4be95005452981c022c0f959902401d3eb2b79
Troy Flavell <hunter007nz@gmail.com>
2021-03-04 22:25:32 +1300

    install router libraries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
+
 
 
 
@@ -15,8 +17,11 @@ class App extends React.Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder /> 
-          <Checkout/>
+          <Switch>
+            <Route path={'/'} exact component={BurgerBuilder} />
+            <Route path={'/checkout'} component={Checkout} />
+            <Route render={() => <h1> component not found</h1>} />
+          </Switch>
         </Layout>
       </div>
     )
