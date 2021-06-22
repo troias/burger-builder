9998795de04719128b8e7ea363f6957f1ff67318
Troy Flavell <hunter007nz@gmail.com>
2021-06-23 00:11:13 +1200

    inc a env file

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,10 +1,12 @@
 import axios from "axios"
 
+const { FIREBASE_API } = process.env;
+
 export const signIn = axios.create({
-    baseURL: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBo5ZKdi2f1enIP10Py-8YLMFTzLHi3r58'
+    baseURL: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API}`
 })
 
 export const signUp = axios.create({
-    baseURL: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBo5ZKdi2f1enIP10Py-8YLMFTzLHi3r58'
+    baseURL: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API}`
 })
 
