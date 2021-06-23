d581f23bbf7b1c44f974a4002e731f10a4e75ec5
Troy Flavell <hunter007nz@gmail.com>
2021-06-23 15:12:42 +1200

    updated

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,12 +1,14 @@
 import axios from "axios"
 
-const { REACT_APP_FIREBASE_API } = process.env;
+const { REACT_APP_FIREBASE_API, FIREBASE_SERVER_KEY } = process.env;
 
 export const signIn = axios.create({
-    baseURL: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${REACT_APP_FIREBASE_API}`
+    baseURL: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${REACT_APP_FIREBASE_API}`, 
+    // headers: { 'Authorization': 'Bearer ' + FIREBASE_SERVER_KEY }
 })
 
 export const signUp = axios.create({
-    baseURL: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${REACT_APP_FIREBASE_API}`
+    baseURL: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${REACT_APP_FIREBASE_API}`, 
+    // headers: { 'Authorization': 'Bearer ' + FIREBASE_SERVER_KEY }
 })
 
